fc6dc3627a0478801947494c7ae24a6e56798614
Naman Sudan <namansudans@gmail.com>
2023-07-09 00:02:00 -0700

    removed unnecessary comments

--- website/__init__.py
+++ website/__init__.py
@@ -10,29 +10,16 @@ from dotenv import load_dotenv
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
-# setting up the database connection
 load_dotenv()
 
-# Retrieve the 'DB_USER' value from the environment variables and store it in the variable DB_USER.
 DB_USER = os.getenv('DB_USER')
-
-# Retrieve the 'DB_PASS' value from the environment variables and store it in the variable DB_PASS.
 DB_PASS = os.getenv('DB_PASS')
-
-# Retrieve the 'DB_HOST' value from the environment variables and store it in the variable DB_HOST.
 DB_HOST = os.getenv('DB_HOST')
-
-# Retrieve the 'DB_PORT' value from the environment variables and store it in the variable DB_PORT.
 DB_PORT = os.getenv('DB_PORT')
-
-# Retrieve the 'DB_NAME' value from the environment variables and store it in the variable DB_NAME.
 DB_NAME = os.getenv('DB_NAME')
 
-
-# function to create the app
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = "helloworld"
     app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
--- website/models.py
+++ website/models.py
@@ -1,19 +1,10 @@
-# Description: This file is going to contain all the database models that we are going to use in our website
-# this imports the database from the __init__.py file
 from . import db 
-# this imports the login manager from the __init__.py file
 from flask_login import UserMixin
-# this imports the login manager from the __init__.py file
 from sqlalchemy.sql import func 
 
-# Define the blog post model
 class Post(db.Model):
-    # this is the id of the post
     id = db.Column(db.Integer, primary_key=True)
-    # this is the title of the post
     title = db.Column(db.String(100))
-    # this is the content of the post
     content = db.Column(db.Text)
-    # this is the date of the post
     date = db.Column(db.DateTime(timezone=True), default=func.now())
 
