1bed0b09412560823b4788a2b421696ccc557236
Naman Sudan <namansudans@gmail.com>
2023-07-14 16:46:06 -0700

    added user model

--- website/models.py
+++ website/models.py
@@ -9,3 +9,18 @@ class Post(db.Model):
     content = db.Column(db.Text)
     date = db.Column(db.DateTime(timezone=True), default=func.now())
 
+class User(db.Model, UserMixin):
+    # Model is  just a table basically
+    # UserMixin will allow us to use the login manager
+    
+    # this is the id of the user. primary_key means that it is unique
+    id = db.Column(db.Integer, primary_key = True)
+    # this is the email of the user. unique means that it is unique
+    email = db.Column(db.String(150), unique = True)
+    # this is the username of the user. unique means that it is unique
+    username = db.Column(db.String(150), unique = True)
+    # this is the password of the user
+    password = db.Column(db.String(150))
+    # this is the date that the user was created
+    data_created = db.Column(db.DateTime(timezone=True), default=func.now())
+
